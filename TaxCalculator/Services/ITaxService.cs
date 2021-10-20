b0d241e6db71ffb6522326318bb2cef43983f520
French, Rich <frenchr1991@gmail.com>
2021-10-20 10:07:01 -0400

    Incomplete comments

--- TaxCalculator/Services/ITaxService.cs
+++ TaxCalculator/Services/ITaxService.cs
@@ -5,15 +5,23 @@ using TaxCalculator.Models;
 
 namespace TaxCalculator.Services
 {
+	/// <summary>
+	/// Defines Service for grabbing taxes from an underlying Tax Calculator
+	/// </summary>
 	public interface ITaxService
 	{
 
 		/// <summary>
-		/// Gets
+		/// Gets Tax rates for given location
 		/// </summary>
 		/// <returns></returns>
 		Rates GetTaxRates(Location taxLocation);
 
+		/// <summary>
+		/// Gets total tax amount for given location and order amount
+		/// </summary>
+		/// <param name="orderToTax"></param>
+		/// <returns></returns>
 		TaxReturn GetTaxForOrder(Order orderToTax);
 
 	}
